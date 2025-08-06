3b7301f046792d39ba16dba9f3ed50c043113bdb
Alexis Lucattini <Alexis.Lucattini@umccr.org>
2025-08-06 16:20:11 +1000

    Fix presigning package lambda

--- app/lambdas/get_dynamodb_evaluated_key_list_py/get_dynamodb_evaluated_key_list.py
+++ app/lambdas/get_dynamodb_evaluated_key_list_py/get_dynamodb_evaluated_key_list.py
@@ -8,16 +8,21 @@ from typing import List, Union, Optional, Dict
 if typing.TYPE_CHECKING:
     from mypy_boto3_dynamodb import DynamoDBClient
     from mypy_boto3_dynamodb.type_defs import AttributeValueTypeDef
+
     PaginationKeyType = Dict[str, 'AttributeValueTypeDef']
+
 
 def get_dynamodb_client() -> 'DynamoDBClient':
     return boto3.client('dynamodb')
 
+
 def get_table_name() -> str:
     return environ['PACKAGING_TABLE_NAME']
 
+
 def get_index_name() -> str:
     return environ['CONTEXT_INDEX_NAME']
+
 
 def get_evaluated_key_list_in_package_query(package_id: str, chunk_size: int = 100) -> List[Union[None, str]]:
     starting_item = True
@@ -42,7 +47,7 @@ def get_evaluated_key_list_in_package_query(package_id: str, chunk_size: int = 1
                 },
                 ExpressionAttributeValues={
                     ":value": {
-                        "S": package_id
+                        "S": f"{package_id}__file"
                     }
                 },
                 Limit=chunk_size
@@ -57,7 +62,7 @@ def get_evaluated_key_list_in_package_query(package_id: str, chunk_size: int = 1
                 },
                 ExpressionAttributeValues={
                     ":value": {
-                        "S": package_id
+                        "S": f"{package_id}__file"
                     }
                 },
                 ExclusiveStartKey=exclusive_start_key,
@@ -75,7 +80,6 @@ def get_evaluated_key_list_in_package_query(package_id: str, chunk_size: int = 1
 
 
 def handler(event, context):
-
     # Get inputs
     packaging_id = event.get("packagingJobId")
     chunk_sizes = event.get("chunkSizes")
@@ -87,3 +91,51 @@ def handler(event, context):
             chunk_size=chunk_sizes
         )
     }
+
+
+# if __name__ == "__main__":
+#     from os import environ
+#     import json
+#
+#     environ['AWS_PROFILE'] = 'umccr-production'
+#     environ['PACKAGING_TABLE_NAME'] = 'DataSharingPackagingLookupTable'
+#     environ['CONTEXT_INDEX_NAME'] = 'context'
+#
+#     print(json.dumps(
+#         handler(
+#             {
+#                 "packagingJobId": "pkg.01K1W1ZF1JW9M5Z5N6TVNRJD3M",
+#                 "chunkSizes": 50
+#             },
+#         None
+#         ),
+#         indent=4
+#     ))
+#
+#     # {
+#     #     "startKeyList": [
+#     #         null,
+#     #         {
+#     #             "id": {
+#     #                 "S": "01938922-7776-7f41-8de4-7eed55a30faf"
+#     #             },
+#     #             "context": {
+#     #                 "S": "pkg.01K1W1ZF1JW9M5Z5N6TVNRJD3M__file"
+#     #             },
+#     #             "job_id": {
+#     #                 "S": "pkg.01K1W1ZF1JW9M5Z5N6TVNRJD3M"
+#     #             }
+#     #         },
+#     #         {
+#     #             "id": {
+#     #                 "S": "01938aad-c795-7551-8436-b9a2eeb5c455"
+#     #             },
+#     #             "job_id": {
+#     #                 "S": "pkg.01K1W1ZF1JW9M5Z5N6TVNRJD3M"
+#     #             },
+#     #             "context": {
+#     #                 "S": "pkg.01K1W1ZF1JW9M5Z5N6TVNRJD3M__file"
+#     #             }
+#     #         }
+#     #     ]
+#     # }
