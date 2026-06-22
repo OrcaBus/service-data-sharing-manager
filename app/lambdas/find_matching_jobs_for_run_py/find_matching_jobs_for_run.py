d7367c4b0bbfe18749f25067d81726ee6638592d
Felipe Jimenez-Ibarra <felipejimenezibarra@gmail.com>
2026-06-22 16:18:59 +1000

    Remove deduplicate dataTypeList.

--- app/lambdas/find_matching_jobs_for_run_py/find_matching_jobs_for_run.py
+++ app/lambdas/find_matching_jobs_for_run_py/find_matching_jobs_for_run.py
@@ -93,7 +93,7 @@ def handler(event, context):
             "packageName": f"{merged_df_group_iter_['jobName'].unique().item()}-{instrument_run_id}",
             "packageRequest": {
                 "libraryIdList": merged_df_group_iter_['libraryId'].tolist(),
-                "dataTypeList": merged_df_group_iter_["dataTypeList"].tolist(),
+                   "dataTypeList": merged_df_group_iter_["dataTypeList"].unique().item(),
                 "instrumentRunIdList": [instrument_run_id],
             },
         }
